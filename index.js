6fd0f5e3b31dd59ae677d70e96e778df3e109c97
unosyceros <85531043+unosyceros@users.noreply.github.com>
2023-01-02 21:22:13 -0300

    prueba

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const router = require('./routes/router')
-const PORT = process.env.PORT || 7070
+const PORT = process.env.PORT || 80
 const app = express();
 
 app.get('/', async (req, res) => {
